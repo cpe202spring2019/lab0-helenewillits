cc3557300d0e79ed1647eccc2057670a9b4ab742
Helene Willits <49109622+helenewillits@users.noreply.github.com>
2019-04-03 12:59:47 -0700

    Update planets.py

--- planets.py
+++ planets.py
@@ -1,7 +1,18 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
-if __name__ == '__main__':
-   weight_on_planets()
+    # input: the user's weight as an integer or float
+    # output: the user's weight on Mars and on Jupiter
+    validInput = False
+    #while loop checks that the input is a number
+    while(validInput == False):
+        try:
+            earth = int(input('What do you weigh on earth? '))
+        except ValueError:
+            pass
+        else:
+            validInput = True
+
+    #calculate weight on other planets
+    mars = earth * 0.38
+    jupiter = earth * 2.34
+
+    print("\nOn Mars you would weigh {0} pounds.".format(mars) + "\nOn Jupiter you would weigh {0} pounds.".format(jupiter))
